8057902679ea465e8f75a19d9d852629bc354c59
CristianRamirez <cristianorcjrl@gmail.com>
2025-05-08 09:40:42 -0500

    Q

---
+++ script.js
@@ -0,0 +1,6 @@
+// No es necesario agregar JS adicional, pero si quieres interactuar con el carrusel puedes usar el siguiente código:
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Carrusel cargado con éxito.");
+    // Aquí puedes agregar funciones personalizadas si lo deseas.
+  });
+  
